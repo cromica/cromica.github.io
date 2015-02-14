5ce9bdc8174eef952c5a3f470c1a23c958227b40
Romulus Crisan <cromica@gmail.com>
2015-02-14 20:57:26 +0200

    added minify _site task in rake

--- Rakefile.rb
+++ Rakefile.rb
@@ -17,6 +17,29 @@ task :preview do
 
 end
 
+# Courtesy of https://github.com/pacbard/blog/blob/master/_rake/minify.rake
+desc "Minify _site/"
+task :minify do
+  puts "\n## Compressing static assets"
+  original = 0.0
+  compressed = 0
+  Dir.glob("_site/**/*.*") do |file|
+    case File.extname(file)
+      when ".css", ".gif", ".html", ".jpg", ".jpeg", ".js", ".png", ".xml"
+        puts "Processing: #{file}"
+        original += File.size(file).to_f
+        min = Reduce.reduce(file)
+        File.open(file, "w") do |f|
+          f.write(min)
+        end
+        compressed += File.size(file)
+      else
+        puts "Skipping: #{file}"
+      end
+  end
+  puts "Total compression %0.2f\%" % (((original-compressed)/original)*100)
+end
+
 
 desc "Build _site/ for development"
   task :dev do
