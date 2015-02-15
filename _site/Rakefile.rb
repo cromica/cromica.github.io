eb6185aea94d14dae1cf039e14b8a4433b01fce8
Romulus Crisan <cromica@gmail.com>
2015-02-15 07:50:09 +0200

    added require dependency to rake

--- _site/Rakefile.rb
+++ _site/Rakefile.rb
@@ -1,3 +1,4 @@
+require "reduce"
 
 desc "Delete _site"
 task :delete do 
@@ -15,6 +16,29 @@ task :preview do
   $?
   p output
 
+end
+
+# Courtesy of https://github.com/pacbard/blog/blob/master/_rake/minify.rake
+desc "Minify _site/"
+task :minify do
+  puts "\n## Compressing static assets"
+  original = 0.0
+  compressed = 0
+  Dir.glob("_site/**/*.*") do |file|
+    case File.extname(file)
+      when ".css", ".gif", ".html", ".jpg", ".jpeg", ".js", ".png", ".xml"
+        puts "Processing: #{file}"
+        original += File.size(file).to_f
+        min = Reduce.reduce(file)
+        File.open(file, "w") do |f|
+          f.write(min)
+        end
+        compressed += File.size(file)
+      else
+        puts "Skipping: #{file}"
+      end
+  end
+  puts "Total compression %0.2f\%" % (((original-compressed)/original)*100)
 end
 
 
